1d30d94782937239eb80c685e95e22369b10e376
isoala <ismaeelalarcon@proton.me>
2025-04-18 19:12:34 -0400

    screen portability fixed for md size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ const Home = () => {
       "font-['var(--font-inter)'] font-feature-settings-['ss03','cv02','cv11']"
     )}>
 
-      <div className="w-full md:w-[45%] p-10 md:p-20 flex flex-col items-start md:sticky md:top-10 md:h-screen md:mt-5"><div>
+      <div className="w-full md:w-[45%] p-10 md:p-20 flex flex-col items-start md:sticky md:top-10 md:h-screen md:mt-5 sm:sticky sm:h-[35vh]"><div>
         <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl flex">
           <Link href="/">Abdurahman Alarcon</Link>
         </h1>
@@ -100,8 +100,8 @@ const Home = () => {
           </ul>
         </nav>
       </div>
-      <div className="mt-60 lg:mt-60 sm:mt-6 hidden md:block">
-      <ul className="ml-1 mt-8 items-center flex" aria-label="Social media">
+        <div className="mt-60 lg:mt-60 sm:mt-6 hidden md:block">
+          <ul className="ml-1 mt-8 items-center flex" aria-label="Social media">
             <li className="mr-5 shrink-0 text-xs">
               <a
                 className="block hover:text-slate-200"
@@ -178,7 +178,7 @@ const Home = () => {
         <div
           ref={scrollContainerRef}
           className={cn(
-            "overflow-y-auto flex-grow",
+            "overflow-y-clip flex-grow",
             "relative"
           )}
         >
@@ -187,7 +187,7 @@ const Home = () => {
 
             <section className=" mt-8 pt-10 text-left" id="about">
 
-              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0">
+              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 dark:bg-gray-900 md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0">
                 <h2 className="text-sm font-bold uppercase tracking-widest text-slate-300">About</h2>
               </div>
               <div className=" md:mt-10 lg:mt-5">
@@ -201,7 +201,7 @@ const Home = () => {
 
 
             <section id="school" className="space-y-4">
-              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0">
+              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 dark:bg-gray-900 md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0">
                 <h2 className="text-sm font-bold uppercase tracking-widest text-slate-300">School</h2>
               </div>
 
@@ -210,7 +210,7 @@ const Home = () => {
                 events={[
                   {
                     date: '2018-2022',
-                    description: 'AP student, team lead on Robotics FRC team 2791, Student, president of Coding club, VP of Future Business Leaders of America, President of Chess Club',
+                    description: 'AP student, team lead on Robotics FRC team 2791, president of Coding club, President of Chess Club',
                     id: '1',
                     title: 'Hayfield Secondary School'
                   },
@@ -254,7 +254,7 @@ const Home = () => {
 
             <section id="experience" className="space-y-2">
 
-              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0">
+              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 dark:bg-gray-900 md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0">
                 <h2 className="text-sm font-bold uppercase tracking-widest text-slate-300">Experience</h2>
               </div>
               <div>
@@ -362,7 +362,7 @@ group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 m
 
 
             <section id="resume" className="space-y-4">
-              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0">
+              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 dark:bg-gray-900 md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0">
                 <h2 className="text-sm font-bold uppercase tracking-widest text-slate-300">Resume</h2>
               </div>
               <a className="inline-flex items-baseline leading-tight hover:text-teal-300 focus-visible:text-teal-300 font-semibold text-slate-200 group/link text-base" href="/resume.pdf" target="_blank" rel="noreferrer noopener" aria-label="View Full Résumé (opens in a new tab)"><span>View Full <span className="inline-block">Résumé<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true"><path fillRule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clipRule="evenodd"></path></svg></span></span></a>
@@ -372,7 +372,7 @@ group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 m
 
             <section id="projects" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="Selected projects">
 
-              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0">
+              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 dark:bg-gray-900 md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0">
                 <h2 className="text-sm font-bold uppercase tracking-widest text-slate-300">Projects</h2>
               </div>
               <div>
@@ -449,14 +449,11 @@ group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 m
                 </ul>
                 <div className="mt-12">
                   <a className="inline-flex items-center font-medium leading-tight text-slate-200 group" aria-label="View Full Project Archive" href="/archive">
-
                   </a>
                 </div>
               </div>
             </section>
-            {/**section 6 */}
-
-            <section id="fin" className="space-y-50">
+            <section id="fin" className="space-y-20">
               <p
                 ref={setElementRef}
                 className="text-base text-gray-600 dark:text-gray-400 transition-opacity">
@@ -473,7 +470,6 @@ group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 m
             </section>
           </div>
         </div>
-        {/* Bottom Fixed Area */}
       </div>
     </div>
 
