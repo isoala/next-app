8a9b8bc0bc091c5d1243ccab445a9dc0cbca211f
isoala <ismaeelalarcon@proton.me>
2025-04-19 12:29:56 -0400

    inverted background color fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,12 +44,12 @@ const Home = () => {
   return (
 
     <div className={cn(
-      "flex flex-col md:flex-row min-h-screen dark:bg-gray-900",
+      "flex flex-col md:flex-row min-h-screen bg-gray-900",
       systemFonts,
       "font-['var(--font-inter)'] font-feature-settings-['ss03','cv02','cv11']"
     )}>
 
-      <div className="w-full md:w-[45%] pt-4 pl-10 pr-10 md:p-20 flex flex-col items-start md:top-10 md:mt-5 sm:sticky overflow-y-clip dark:bg-gray-900"><div>
+      <div className="w-full md:w-[45%] pt-4 pl-10 pr-10 md:p-20 flex flex-col items-start md:top-10 md:mt-5 sm:sticky overflow-y-clip bg-gray-900"><div>
         <h1 className="text-4xl font-bold tracking-tight text-slate-200 md:text-5xl flex ">
           <Link href="/">Abdurahman Alarcon</Link>
         </h1>
@@ -174,7 +174,7 @@ const Home = () => {
 
             <section className=" mt-8 pt-10 text-left" id="about">
 
-              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 dark:bg-gray-900 md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0">
+              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 bg-gray-900 md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0">
                 <h2 className="text-sm font-bold uppercase tracking-widest text-slate-300">About</h2>
               </div>
               <div className=" md:mt-10 lg:mt-5">
@@ -187,13 +187,13 @@ const Home = () => {
             </section>
 
 
-            <section id="school" className="space-y-4 dark:bg-gray-900">
-              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 dark:bg-gray-900 md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0">
+            <section id="school" className="space-y-4 bg-gray-900">
+              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 bg-gray-900 md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0">
                 <h2 className="text-sm font-bold uppercase tracking-widest text-slate-300">School</h2>
               </div>
 
               <AnimatedTimelinePage
-                containerClassName="bg-gray-100 dark:bg-gray-900 hover:text-white transition-opacity font-medium focus-visible:text-white-500 transition duration-200 "
+                containerClassName="bg-gray-100 bg-gray-900 hover:text-white transition-opacity font-medium focus-visible:text-white-500 transition duration-200 "
                 events={[
                   {
                     date: '2018-2022',
@@ -238,9 +238,9 @@ const Home = () => {
               />
             </section>
 
-            <section id="experience" className="space-y-2 dark:bg-gray-900">
+            <section id="experience" className="space-y-2 bg-gray-900">
 
-              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 dark:bg-gray-900 md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0">
+              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 bg-gray-900 md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0">
                 <h2 className="text-sm font-bold uppercase tracking-widest text-slate-300">Experience</h2>
               </div>
               <div>
@@ -342,14 +342,14 @@ group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 m
                 </ol>
               </div>
             </section>
-            <section id="resume" className="space-y-4 dark:bg-gray-900">
-              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 dark:bg-gray-900 md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0">
+            <section id="resume" className="space-y-4 bg-gray-900">
+              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 bg-gray-900 md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0">
                 <h2 className="text-sm font-bold uppercase tracking-widest text-slate-300">Resume</h2>
               </div>
               <a className="inline-flex items-baseline leading-tight hover:text-teal-300 focus-visible:text-teal-300 font-semibold text-slate-200 group/link text-base" href="/resume.pdf" target="_blank" rel="noreferrer noopener" aria-label="View Full Résumé (opens in a new tab)"><span>View Full <span className="inline-block">Résumé<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true"><path fillRule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clipRule="evenodd"></path></svg></span></span></a>
             </section>
-            <section id="projects" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24 dark:bg-gray-900" aria-label="Selected projects">
-              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 dark:bg-gray-900 md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0">
+            <section id="projects" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24 bg-gray-900" aria-label="Selected projects">
+              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 bg-gray-900 md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0">
                 <h2 className="text-sm font-bold uppercase tracking-widest text-slate-300">Projects</h2>
               </div>
               <div>
@@ -428,7 +428,7 @@ group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 m
                 </div>
               </div>
             </section>
-            <section id="fin" className="space-y-20 dark:bg-gray-900">
+            <section id="fin" className="space-y-20 bg-gray-900">
               <p
                 ref={setElementRef}
                 className="text-base text-gray-600 dark:text-gray-400 transition-opacity">
