6800011fb9fdacc56dde56ca0d5a56fd7ba93b09
isoala <ismaeelalarcon@proton.me>
2025-04-18 20:08:32 -0400

    removed unecessary code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,10 @@ const systemFonts = 'ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,"S
 const Home = () => {
   const scrollContainerRef = useRef<HTMLDivElement>(null);
   const textElementsRef = useRef<HTMLElement[]>([]);
-  const lastY = useRef<number | undefined>(undefined);
+  const disappearingDivRef = useRef<HTMLDivElement>(null);
+  const previousDivRef = useRef<HTMLDivElement>(null);
+
+
 
   useEffect(() => {
     const scrollContainer = scrollContainerRef.current;
@@ -33,72 +36,19 @@ const Home = () => {
         threshold: [0, 0.1, 0.9, 1],
       }
     );
-
+    
 
     textElementsRef.current.forEach((el) => observer.observe(el));
 
-    const handleScroll = (event: WheelEvent) => {
-      const { scrollTop, clientHeight, scrollHeight } = scrollContainer;
-
-      if (scrollTop === 0 && event.deltaY < 0) {
-        document.body.style.overflow = 'auto';
-      } else if (scrollTop + clientHeight === scrollHeight && event.deltaY > 0) {
-        document.body.style.overflow = 'auto';
-      } else {
-        document.body.style.overflow = 'hidden';
-      }
-    };
-
-    const handleTouchMove = (event: TouchEvent) => {
-      const { scrollTop, clientHeight, scrollHeight } = scrollContainer;
-      const touch = event.touches[0];
-      const currentY = touch.clientY;
-
-      if (typeof lastY.current !== 'undefined') {
-        const deltaY = currentY - lastY.current;
-
-        if (scrollTop === 0 && deltaY > 0) {
-          document.body.style.overflow = 'auto';
-        } else if (scrollTop + clientHeight === scrollHeight && deltaY < 0) {
-          document.body.style.overflow = 'auto';
-        } else {
-          document.body.style.overflow = 'hidden';
-        }
-      }
-      lastY.current = currentY;
-    };
-
-    const handleTouchStart = (event: TouchEvent) => {
-      if (event.touches && event.touches.length === 1) {
-        lastY.current = event.touches[0].clientY;
-      }
-    };
-
-    const handleTouchEnd = () => {
-      lastY.current = undefined;
-    };
-
-    scrollContainer.addEventListener('wheel', handleScroll, { passive: false });
-    scrollContainer.addEventListener('touchstart', handleTouchStart, { passive: false });
-    scrollContainer.addEventListener('touchmove', handleTouchMove, { passive: false });
-    scrollContainer.addEventListener('touchend', handleTouchEnd, { passive: true });
-
     return () => {
       observer.disconnect();
-      scrollContainer.removeEventListener('wheel', handleScroll);
-      scrollContainer.removeEventListener('touchstart', handleTouchStart);
-      scrollContainer.removeEventListener('touchmove', handleTouchMove);
-      scrollContainer.removeEventListener('touchend', handleTouchEnd);
-      document.body.style.overflow = 'auto';
     };
   }, []);
-
   const setElementRef = (el: HTMLElement | null) => {
     if (el) {
       textElementsRef.current.push(el);
     }
   };
-
 
   return (
 
@@ -108,8 +58,8 @@ const Home = () => {
       "font-['var(--font-inter)'] font-feature-settings-['ss03','cv02','cv11']"
     )}>
 
-      <div className="w-full md:w-[45%] p-10 md:p-20 flex flex-col items-start md:sticky md:top-10 md:mt-5 sm:sticky md:h-[15vh]"><div>
-        <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl flex">
+      <div className="w-full md:w-[45%] p-10 sm:p-20 flex flex-col items-start md:top-10 sm:mt-5 sm:sticky overflow-y-clip"><div>
+        <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl flex ">
           <Link href="/">Abdurahman Alarcon</Link>
         </h1>
         <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-300 sm:text-xl flex">
@@ -229,13 +179,7 @@ const Home = () => {
       {/* section 2 */}
 
       <div className="w-full md:w-[55%] flex flex-col h-screen overflow-y-auto overflow-x-hidden">
-        <div
-          ref={scrollContainerRef}
-          className={cn(
-            "overflow-y-clip flex-grow",
-            "relative"
-          )}
-        >
+        
           <div className="p-10 space-y-12 space-x-10">
 
 
@@ -304,7 +248,6 @@ const Home = () => {
                 title=" "
               />
             </section>
-            {/* section 3 */}
 
             <section id="experience" className="space-y-2">
 
@@ -409,23 +352,14 @@ group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 m
                   </li>
                 </ol>
               </div>
-
             </section>
-            {/* section 4 */}
-
-
-
             <section id="resume" className="space-y-4">
               <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 dark:bg-gray-900 md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0">
                 <h2 className="text-sm font-bold uppercase tracking-widest text-slate-300">Resume</h2>
               </div>
               <a className="inline-flex items-baseline leading-tight hover:text-teal-300 focus-visible:text-teal-300 font-semibold text-slate-200 group/link text-base" href="/resume.pdf" target="_blank" rel="noreferrer noopener" aria-label="View Full Résumé (opens in a new tab)"><span>View Full <span className="inline-block">Résumé<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true"><path fillRule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clipRule="evenodd"></path></svg></span></span></a>
             </section>
-
-
-
             <section id="projects" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="Selected projects">
-
               <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 dark:bg-gray-900 md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0">
                 <h2 className="text-sm font-bold uppercase tracking-widest text-slate-300">Projects</h2>
               </div>
@@ -494,12 +428,10 @@ group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 m
                           </a>
                         </h3>
                         <p className="mt-2 text-sm leading-normal">Guide for United Stated expatriates stationed in Turkey. The app serves as an easy way to find emergency services, restaurants, and stores in the area. Created using Microsoft PowerApps, and Azure SQL.</p>
-                        {/* Optional: Add links or technologies here */}
                       </div>
                       <Image alt="Placeholder image for project two" loading="lazy" width={200} height={48} decoding="async" className="aspect-video object-cover rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1" style={{ color: 'transparent' }} src="/dos.png" />
                     </div>
                   </li>
-                  {/* Add more project list items here */}
                 </ul>
                 <div className="mt-12">
                   <a className="inline-flex items-center font-medium leading-tight text-slate-200 group" aria-label="View Full Project Archive" href="/archive">
@@ -513,11 +445,11 @@ group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 m
                 className="text-base text-gray-600 dark:text-gray-400 transition-opacity">
                 I built this web application using&nbsp;
                 <Image
-                  src="/next.svg" // Path to your SVG in the public directory
-                  alt="SVG Logo" // Alt text for accessibility
-                  width={100} // Adjust width as needed
-                  height={70} // Adjust height as needed
-                  className="inline-block ml-1 align-middle" // Style to align with text
+                  src="/next.svg" 
+                  alt="SVG Logo"
+                  width={100} 
+                  height={70}
+                  className="inline-block ml-1 align-middle" 
                 />
                 &nbsp; .
               </p>
@@ -525,7 +457,6 @@ group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 m
           </div>
         </div>
       </div>
-    </div>
 
   );
 
