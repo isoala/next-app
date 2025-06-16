0d4f57684fc64ec2d318de3cf39c0adae9fbfb52
isoala <ismaeelalarcon@proton.me>
2025-06-15 23:20:45 -0400

    updated title of job

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -177,7 +177,7 @@ const Home = () => {
                         <div>
                           <a className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base" href="https://www.state.gov/" target="_blank" rel="noreferrer noopener" aria-label="Department of State (opens in a new tab)">
                             <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                            <span>Engineering Intern ·&nbsp;<span className="inline-block">Department of State<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group
+                            <span>DevOps Engineer Intern ·&nbsp;<span className="inline-block">Department of State<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group
 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true"><path fillRule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clipRule="evenodd"></path></svg></span></span>
                           </a>
                         </div>
